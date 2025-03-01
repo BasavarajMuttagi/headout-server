d044c75e60bbc6d1b73601b7fa1004bb5fb91151
BasavarajMuttagi <basavaraj2770@gmail.com>
2025-03-01 18:47:31 +0530

    add @clerk/express middleware and dependency

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-
+import { clerkMiddleware } from "@clerk/express";
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(clerkMiddleware());
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
